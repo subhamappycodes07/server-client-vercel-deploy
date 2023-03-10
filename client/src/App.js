65bab5b60ccd7a84fa1d80d2606623937ff2f1c9
subhamappycodes07 <subham.haldar@appycodes.com>
2023-03-10 17:01:28 +0530

    api config

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ const App = () => {
   const [data, setData] = useState("Data will show here ...")
   const getData = async () => {
     try {
-      const res = await fetch("http://localhost:8080/api/test/")
+      const res = await fetch("https://server-client-vercel-deploy.vercel.app/api/test/")
       const data = await res.json()
       setData(data.message)
 
